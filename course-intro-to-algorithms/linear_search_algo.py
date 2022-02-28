06ee2aadae2b34a0c9536a7ba9ba7c336087b7c9
MohamedHakem <mohamedhakem959628@gmail.com>
2022-02-28 18:27:15 +0200

    Initial commit

---
+++ course-intro-to-algorithms/linear_search_algo.py
@@ -0,0 +1,20 @@
+def linear_search(lst, target):
+    """Returns the index position of the target if found, else returns -1"""
+
+    for i in range(0, len(lst)):
+        if lst[i] == target:
+            return i
+    return -1
+    
+    
+    
+    
+"""
+///////////////////////////////////////////
+Or more cleaned up like this:
+"""
+def linear_search(lst, target):
+    for index, value in enumerate(lst):
+        if value == target:
+            return index
+    return -1
